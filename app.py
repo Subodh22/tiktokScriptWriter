e9e724d4a5a3b7e28b650551e50cf85c2eec2987
Subodh22 <subodhmaharjan33@gmail.com>
2024-04-28 09:51:57 +1000

    added dependcies checking if it work

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
-st.write("Hello World")
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo"
+
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system","You are a friendly assistant named max"),
+    ("human","{input}")
+])
+
+chain= prompt | llm
+
+response =chain.invoke({"input":"What is your name"})
+
+st.write(response["content"])
