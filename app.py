d2ef6f8a6b93e26b1b12bb70a993a592eddd3ed6
Subodh22 <SubodhMaharjan33@gmail.com>
2024-04-28 13:12:00 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ load_dotenv()
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
-# OPENAI_API_KEY=st.secrets["OPENAI_API_KEY"]
+OPENAI_API_KEY=st.secrets["OPENAI_API_KEY"]
 
 llm = ChatOpenAI(
     model="gpt-3.5-turbo"
