5f93b94b0b11686ca691726168f319d7683978d2
Subodh22 <SubodhMaharjan33@gmail.com>
2024-04-28 10:23:04 +1000

    new apikey added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
-
+from langchain_core.output_parsers import StrOutputParser
 OPENAI_API_KEY=st.secrets["OPENAI_API_KEY"]
 
 llm = ChatOpenAI(
@@ -15,9 +15,9 @@ prompt = ChatPromptTemplate.from_messages([
     ("system","You are a friendly assistant named max"),
     ("human","{input}")
 ])
-
-chain= prompt | llm
+outputParser=StrOutputParser()
+chain= prompt | llm | outputParser
 print("ok")
 response =chain.invoke({"input":"What is your name"})
 
-st.write(response["content"])
+st.write(response)
